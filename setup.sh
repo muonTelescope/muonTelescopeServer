9e735f324a7729ac4918c4ef306fa3e0c0fd6777
sawaiz <github@sawaizsyed.com>
2017-04-12 21:40:40 -0400

    Setup file and pelimenary service

---
+++ setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Setup, change hostname, install docker and compose etc...
+
+#Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+read -e -p "Change hostname to: " host
+read -e -s -p "Change Root (pi) Password to: " rootPasswd
+echo ""
+read -e -p "Add User, Username: " userName
+read -e -s -p "$userName Password: " userPasswd
+echo ""
+
+#Change root (pi) password
+echo "pi:$rootPasswd" | chpasswd
+
+#Assign existing hostname to $hostn
+hostn=$(cat /etc/hostname)
+
+#change hostname in /etc/hosts & /etc/hostname
+sudo sed -i "s/$hostn/$host/g" /etc/hosts
+sudo sed -i "s/$hostn/$host/g" /etc/hostname
+
+#Create the user
+useradd -m -G sudo $userName
+echo "$userName:$userPasswd" | chpasswd
+
+#Update/upgrade
+apt-get update && apt-get -y upgrade
+
+#Install docker/docker compose
+curl -sSL https://get.docker.com | sh
+apt-get -y install python-pip
+pip install docker-compose
+
+# enable I2C on Raspberry Pi
+echo '>>> Enable I2C'
+if grep -q 'i2c-bcm2708' /etc/modules; then
+  echo 'Seems i2c-bcm2708 module already exists, skip this step.'
+else
+  echo 'i2c-bcm2708' >> /etc/modules
+fi
+if grep -q 'i2c-dev' /etc/modules; then
+  echo 'Seems i2c-dev module already exists, skip this step.'
+else
+  echo 'i2c-dev' >> /etc/modules
+fi
+if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
+  echo 'Seems i2c1 parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c1=on' >> /boot/config.txt
+fi
+if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
+  echo 'Seems i2c_arm parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+fi
+if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+  sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+  sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+else
+  echo 'File raspi-blacklist.conf does not exist, skip this step.'
+fi
